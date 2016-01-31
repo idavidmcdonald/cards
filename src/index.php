5481a03fd6b1a3ed8968a97b6b860a6c906d3d72
David McDonald <idavidmcdonald@me.com>
2016-01-31 16:37:34 +0000

    Fixed order logic, renamed variables for better readability and improved commenting and user messages

--- src/index.php
+++ src/index.php
@@ -8,45 +8,46 @@ require_once __DIR__ . '/../vendor/autoload.php';
 try {
     // Start game
     $deck = new Cards\Deck();
-    echo 'Ready? Guess correctly for the next 7 cards to win.', PHP_EOL;
+    echo "Ready? Guess correctly for the next 7 cards to win. Don't forget that Ace is low.", PHP_EOL;
 
     // Display first card
     $currentCard = $deck->getNextCard();
     echo 'The first card is the ', $currentCard->getRankAsString(), ' of ', $currentCard->getSuitAsString(), PHP_EOL, PHP_EOL;
 
     // 7 rounds
-    for ($i=0; $i < 7; $i++) { 
+    for ($i = 0; $i < 7; $i++) { 
         // Ask user higher or lower
-        $hl = readline("Will the next card be higher or lower? h/l: ");
+        $userGuess = readline("Will the next card be higher or lower? h/l: ");
 
         // Validate user input
-        if ($hl != 'h' AND $hl != 'l') {
+        if ($userGuess != 'h' AND $userGuess != 'l') {
             echo 'Invalid input, h or l only please!', PHP_EOL;
             exit;
         }
 
         // Draw next card to compare
         $nextCard = $deck->getNextCard();
-        echo PHP_EOL, 'Correct. The next card is the ', $nextCard->getRankAsString(), ' of ', $nextCard->getSuitAsString(), PHP_EOL, PHP_EOL;
 
-        // Determine if next card is higher, lower or the same rank
+        // Determine if next card is higher (h), lower(l) or the same (s) rank
         if ($nextCard->getRank() > $currentCard->getRank()) {
             $answer = 'h';
         } else if ($nextCard->getRank() < $currentCard->getRank()) {
             $answer = 'l';
         } else {
-            // Same rank, draw next card
-            $currentCard = $nextCard;
-            continue;
+            $answer = 's';
         }
 
-        // Check if user guess is incorrect
-        if ($hl != $answer) {
-            echo 'You lost', PHP_EOL;
+        // Show user message if guess was incorrect/correct
+        if ($answer == 's') {
+            echo 'Same rank. The next card was the ', $nextCard->getRankAsString(), ' of ', $nextCard->getSuitAsString(), PHP_EOL, PHP_EOL;
+        } else if ($userGuess == $answer) {
+            echo PHP_EOL, 'Correct. The next card is the ', $nextCard->getRankAsString(), ' of ', $nextCard->getSuitAsString(), PHP_EOL, PHP_EOL;
+        } else {
+            echo 'You lost. The next card was the ', $nextCard->getRankAsString(), ' of ', $nextCard->getSuitAsString(), PHP_EOL, PHP_EOL;
             exit;
         }
 
-        // Correct guess, draw next card
+        // Set up current card for next round
         $currentCard = $nextCard;
     }
     
